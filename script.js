2923ab21ee6cf0ea76693841aa329d6a2583a8fb
madhanyadav2004-cyber <madhanyadav2004@gmail.com>
2026-02-19 13:57:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+// Function to greet a user
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to GitHub 🚀`;
+}
+
+// Example usage
+const userName = "Developer";
+console.log(greetUser(userName));
+
+// Simple calculator function
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Sum:", add(5, 3));
